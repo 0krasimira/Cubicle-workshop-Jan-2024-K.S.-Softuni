80270f2dcaa120df28e460e999e55be7d7f8dcaf
0krasimira <kstoyanova2@gmail.com>
2024-01-16 15:33:26 +0200

    Show cubes on home page dynamically

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,15 +1,20 @@
+const uniqid = require("uniqid")
+
 
 const cubes = []
+
 
 exports.getAll = () => cubes.slice()
 
 exports.create = (cubeData) => {
    const newCube = {
-        id: cubes.length + 1,
+        id: uniqid(),
         ...cubeData
     }
 
     cubes.push(newCube);
+
+
 
     return newCube
 }
