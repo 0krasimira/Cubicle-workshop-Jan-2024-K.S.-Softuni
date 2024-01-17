2e6d0a1f9f6e20e21ec3465991c37b6d6058c7ba
0krasimira <kstoyanova2@gmail.com>
2024-01-17 12:40:07 +0200

    Add single cube details page

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,7 +1,23 @@
 const uniqid = require("uniqid")
 
 
-const cubes = []
+const cubes = [
+    {
+        id: "58sdjbwtlrges7x9",
+        name: "Triangle", 
+        description: "triangular cube", 
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkdAnR3y0rPMsNeYqN90zvAW3ZNXQCsA0C6A&usqp=CAU",
+        difficultyLevel: 800
+    },
+
+    {
+        id: "58sdjvbtlrges432",
+        name: "Oval", 
+        description: "Oval cube", 
+        imageUrl: "https://www.jaapsch.net/puzzles/images/cubeoval.jpg",
+        difficultyLevel: 800
+    }
+]
 
 
 exports.getAll = () => cubes.slice()
@@ -18,3 +34,5 @@ exports.create = (cubeData) => {
 
     return newCube
 }
+
+exports.getOne = (cubeId) => cubes.find(x => x.id === cubeId)
