e8452f9c63566d6966777055e0450d268cabffbb
0krasimira <kstoyanova2@gmail.com>
2024-01-17 13:16:20 +0200

    Implemented Search

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -7,7 +7,7 @@ const cubes = [
         name: "Triangle", 
         description: "triangular cube", 
         imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkdAnR3y0rPMsNeYqN90zvAW3ZNXQCsA0C6A&usqp=CAU",
-        difficultyLevel: 800
+        difficultyLevel: 80
     },
 
     {
@@ -15,12 +15,30 @@ const cubes = [
         name: "Oval", 
         description: "Oval cube", 
         imageUrl: "https://www.jaapsch.net/puzzles/images/cubeoval.jpg",
-        difficultyLevel: 800
+        difficultyLevel: 40
     }
 ]
 
 
-exports.getAll = () => cubes.slice()
+exports.getAll = (search, from, to) => {
+
+    let result = cubes.slice()
+
+    if(search){
+        result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()))
+    }
+
+    if(from){
+        result = result.filter(cube => cube.difficultyLevel >= Number(from))
+    }
+
+    if(to){
+        result = result.filter(cube => cube.difficultyLevel <= Number(to))
+    }
+
+    return result
+}
+
 
 exports.create = (cubeData) => {
    const newCube = {
