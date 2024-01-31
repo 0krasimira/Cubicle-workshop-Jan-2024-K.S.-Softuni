0fa0e2367d675140b97ea98012a2c2508fab66a7
0krasimira <kstoyanova2@gmail.com>
2024-01-31 09:34:46 +0200

    Showed create and details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -6,20 +6,5 @@ router.get('/create', (req, res) => {
     res.render("create")
 })
 
-router.post('/create', (req, res) => {
-    const {name, description, difficultyLevel, imageUrl} = req.body
-    cubeManager.create({name, description, difficultyLevel: Number(difficultyLevel), imageUrl})
-    res.redirect("/")
-})
-
-router.get('/:cubeId/details', (req, res) => {
-    const cube = cubeManager.getOne(req.params.cubeId)
-
-    if(!cube) {
-        return res.redirect("/404")
-    }
-
-    res.render('details', {...cube})
-})
 
 module.exports = router
