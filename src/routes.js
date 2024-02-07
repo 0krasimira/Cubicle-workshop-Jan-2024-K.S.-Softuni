2c4f7d54ec246bc914b7a92060fa7d66d29404e4
0krasimira <kstoyanova2@gmail.com>
2024-02-07 13:23:45 +0200

    Showed register page

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ const router = require("express").Router()
 const homeController = require("./controllers/homeController")
 const cubeController = require("./controllers/cubeController")
 const accessoryController = require('./controllers/accessoryController')
+const authController = require("./controllers/authController")
 
 // Routes
 router.use(homeController)
 router.use('/cubes', cubeController)
 router.use('/accessories', accessoryController)
+router.use('/auth', authController)
 router.get('*', (req, res) => {
     res.redirect('404')
 })
