d37f785e36bcf9c9bcef8b45d020fad656b2cf55
0krasimira <kstoyanova2@gmail.com>
2024-02-07 15:22:23 +0200

    Hash password pre save

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require("bcrypt")
 
 const userSchema = new mongoose.Schema({
     username: String,
@@ -11,6 +12,12 @@ const userSchema = new mongoose.Schema({
         //     message: 'Password missmatch!'
         // }
     },
+})
+
+userSchema.pre("save", async function() {
+    const hash = await bcrypt.hash(this.password, 12)
+    this.password = hash
+    
 })
 
 userSchema.virtual('repeatPassword')
